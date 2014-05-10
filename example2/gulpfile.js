a4bb665183c1a373c55023585f37c11461adcc05
Marat Abdullin <dakota@brokenpipe.ru>
2014-05-11 01:37:33 +0400

    More complete Gulp example.

--- example2/gulpfile.js
+++ example2/gulpfile.js
@@ -2,32 +2,53 @@
 
 var gulp = require('gulp');
 
-var rename = require('gulp-rename');
 var conkitty = require('gulp-conkitty');
 var gulpFilter = require('gulp-filter');
 var concat = require('gulp-concat');
-
-var cssFilter = gulpFilter('**/*.css');
-var jsFilter = gulpFilter('**/!(tpl).js'); // Everything except tpl.js.
+var clean = require('gulp-clean');
 
 
-gulp.task('conkitty', function() {
+gulp.task('conkitty', ['clean'], function() {
+    var cssFilter = gulpFilter('**/*.css');
+    var jsFilter = gulpFilter(['**/*.js', '!tpl.js']); // Everything except tpl.js.
+
     return gulp.src(['./src/**/*.ctpl'])
-        //.pipe(rename('babydom.min.js'))
         .pipe(conkitty({
             common: 'common.js', // Filename for templates commons.
             templates: 'tpl.js', // Filename for compiled templates.
             deps: true // Append external templates dependencies to the result.
         }))
-        .pipe(cssFilter)// Concat all css files to bundle deps.css.
-        .pipe(concat('deps.css'))
+
+        // As the result of Conkitty plugin we get templates commons
+        // (in common.js), compiled templates themselves (in tpl.js), and
+        // declared in templates (because deps setting is true) dependencies.
+        .pipe(cssFilter)
+        .pipe(concat('deps.css')) // Concat all css files to bundle deps.css.
         .pipe(cssFilter.restore())
+
         .pipe(jsFilter)
-        .pipe(concat('deps.js')) // Concat all js files except for tpl.js to
-                                 // bundle deps.js
+        .pipe(concat('deps.js')) // Concat all js files except for tpl.js to bundle deps.js.
         .pipe(jsFilter.restore())
+
+        .pipe(gulp.dest('./dst')); // Copy deps.css, deps.js and tpl.js to dst.
+});
+
+
+gulp.task('copy-index-html', ['clean'], function() {
+    return gulp.src('./src/index.html')
         .pipe(gulp.dest('./dst'));
 });
 
 
-gulp.task('default', ['conkitty']);
+gulp.task('clean', function() {
+    gulp.src('./dst')
+        .pipe(clean());
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('./src/**/*', ['clean', 'copy-index-html', 'conkitty']);
+});
+
+
+gulp.task('default', ['clean', 'copy-index-html', 'conkitty']);
